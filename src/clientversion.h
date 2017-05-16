82192a8d06d0c4343ae464d2d38415d10a484ea3
chaincoin <chaincoin@users.noreply.github.com>
2017-05-16 12:38:53 +0200

    Chaincoin versions

--- src/clientversion.h
+++ src/clientversion.h
@@ -17,16 +17,16 @@
 #define CLIENT_VERSION_MAJOR 0
 #define CLIENT_VERSION_MINOR 12
 #define CLIENT_VERSION_REVISION 0
-#define CLIENT_VERSION_BUILD 60
+#define CLIENT_VERSION_BUILD 0
 
 //! Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE true
+#define CLIENT_VERSION_IS_RELEASE false
 
 /**
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2016
+#define COPYRIGHT_YEAR 2017
 
 #endif //HAVE_CONFIG_H
 
