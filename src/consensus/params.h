19e8074e454e9c042de691e85e8a0ad3c99aa7d5
Michael Polzer <michael@298-MBP-MIP-3.local>
2017-11-17 17:38:13 +0300

    add variables for Chaincoin

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -79,6 +79,9 @@ struct Params {
     bool fPowNoRetargeting;
     int64_t nPowTargetSpacing;
     int64_t nPowTargetTimespan;
+    int64_t nInterval;
+    int64_t nAveragingInterval;
+    int64_t AveragingTargetTimespan() const { return nAveragingInterval * nPowTargetSpacing; }
     int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
     uint256 nMinimumChainWork;
     uint256 defaultAssumeValid;
